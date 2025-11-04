ef6d445a0b5d71518cff27486da5bde88a83fb08
Avi <avipars@users.noreply.github.com>
2025-11-04 14:53:24 -0500

    tone pitch follower

---
+++ Basics/Potentiometer/ToneFollow.ino
@@ -0,0 +1,23 @@
+// C++ code
+// https://www.tinkercad.com/things/9MfBnuEXFKN-tonepitchfollowpotent
+void setup()
+{
+   Serial.begin(9600); 
+
+}
+
+void loop()
+{
+  // read the sensor:
+ int sensorReading = analogRead(A0);
+ // print the sensor reading so you know its range
+ Serial.println(sensorReading);
+ // map the analog input range (in this case, 400 - 1000 from the photoresistor)
+ // to the output pitch range (120 - 1500Hz)
+ // change the minimum and maximum input numbers below depending on the range
+ // your sensor's giving:
+ int thisPitch = map(sensorReading, 400, 1000, 120, 1500);
+ // play the pitch:
+ tone(9, thisPitch, 10);
+ delay(1); // delay in between reads for stability
+}
